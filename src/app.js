21aeaa7fe0163af198807d8fd32c50bcaf93724b
Ansh Patel <anshpatel4625@gmail.com>
2026-09-19 09:58:19 +0530

    setup for writing controller

--- src/app.js
+++ src/app.js
@@ -16,4 +16,12 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+
+// import router
+import userRouter from './routes/user.routes.js'
+
+// router declaration
+app.use("/api/v1/users", userRouter)
+
+
 export { app }
